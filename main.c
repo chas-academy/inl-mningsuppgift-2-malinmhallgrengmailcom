7618f771d0a183e46f4a1cb5adcd1d700d053038
malin.m.hallgren@gmail.com <malin.m.hallgren@gmail.com>
2025-03-29 11:17:12 +0100

    Calculations and comments for Celsius to Fahrenheit and vice versa

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
 
-// This is where you add the two functions
+// function to convert F to C
+float fahrenheit_to_celsius(float farenheit) {
+    float calculated_celsius = 0;
+
+    calculated_celsius = (farenheit - 32) * 5 / 9;
+
+    return calculated_celsius;
+}
+
+//function to convert C to F
+float celsius_to_fahrenheit(float celsius) {
+    float calculated_fahrenheit = (celsius * 9) / 5 + 32;
+
+    return calculated_fahrenheit;
+}
 
 int main() {
     float temp_f = 100.0;
